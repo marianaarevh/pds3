f6329884f0e6c2ed755c365403effe1579a9d01d
marianaarevh <marianaarevalo933@gmail.com>
2023-08-13 12:48:42 -0700

    primera tarea

---
+++ grados.js
@@ -0,0 +1,29 @@
+console.log("- TEMPERATURA -")
+var gradosC=25;
+var gradosF=32+(9*gradosC/5); 
+console.log(gradosC + "ºC equivalen a " + gradosF + "ºF");
+
+console.log("- CIRCUNFERENCIA -")
+ var radio=12;
+ const pi = 3.1416;
+ var longitud= 2*pi*radio;
+ var area=pi*radio*radio;
+ console.log("Radio = " + radio)
+ console.log("Longitud de la circunferencia = " + longitud)
+ console.log("Area de la circunferencia = " + area)
+
+ console.log("- VELOCIDAD -")
+ var kmh=100;
+ var ms = (kmh * (5/18)).toFixed(2);
+ console.log(kmh + "km/h equivalen a " + ms + "m/s");
+
+ console.log("- PITAGORAS -")
+var a= 20;
+var b= 30;
+var c = Math.sqrt((a*a)+(b*b)).toFixed(2);
+console.log("Cateto a = " +a);
+console.log("Cateto b = " +b);
+console.log("Hipotenusa = " +c);
+
+console.log("- MATRIZ INVERSA -")
+
